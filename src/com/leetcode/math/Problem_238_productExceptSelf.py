864df63d4fba854f4940a8aee82b46fc96321134
StringsLi <lixin4009@163.com>
2020-06-05 16:44:32 +0800

    no message

---
+++ src/com/leetcode/math/Problem_238_productExceptSelf.py
@@ -0,0 +1,18 @@
+from typing import List
+
+"""
+基于左侧和右侧和来计算
+"""
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        L = [1] * len(nums)
+        R = [1] * len(nums)
+
+        for i in range(1, len(nums)):
+            L[i] = nums[i - 1] * L[i - 1]
+        for j in range(len(nums) - 2, -1, -1):
+            R[j] = nums[j + 1] * R[j + 1]
+        res = [0] * len(nums)
+        for i in range(0, len(nums)):
+            res[i] = L[i] * R[i]
+        return res
--- src/com/target_offer/Problem_20_isNumber.py
+++ src/com/target_offer/Problem_20_isNumber.py
@@ -5,3 +5,9 @@ class Solution:
             return True
         except:
             return False
+
+
+if __name__ == '__main__':
+    obj = Solution()
+    s = "-1E-16"
+    print(obj.isNumber(s))
